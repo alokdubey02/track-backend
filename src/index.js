057f15802c13ba724c0215c0a20dbf1d6436cea0
alokdubey02 <alok@ebetainnovations.com>
2025-05-15 19:04:43 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,41 @@
+require("dotenv").config();
+require("./models/User");
+require("./models/Track");
+
+const express = require("express");
+const mongoose = require("mongoose");
+
+const bodyParser = require("body-parser");
+
+const authRoutes = require("./routes/authRoutes");
+const trackRoutes = require("./routes/trackRoutes");
+
+const requireAuth = require("./middlewares/requireAuth");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(authRoutes);
+app.use(trackRoutes);
+
+const mongoUri = process.env.MONGO_URI;
+
+mongoose.connect(mongoUri);
+
+mongoose.connection.on("connected", () => {
+  console.log("Connected to mongo instance");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log("error argument ", err);
+});
+
+app.get("/", requireAuth, (req, res) => {
+  res.send(`Your email: ${req.user.email} ${req.user}`);
+});
+// , "192.168.183.104",
+app.listen(3000, "192.168.1.4", () => {
+  // app.listen(3000, "0.0.0.0", () => {
+  console.log("Backend running on port 3000");
+});
